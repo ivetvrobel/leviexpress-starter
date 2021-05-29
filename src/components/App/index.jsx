8789ce25afb18121d462f5e14f739f9f7630032e
ivetvrobel <iveta.vrobelova@gmail.com>
2021-05-29 10:35:19 +0200

    Spuštěno routování mezi stránkami Home a  Reservation

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
+    <Router>
+        <Header />
+
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            <Route path="/reservation">
+                <Reservation />
+            </Route>
+        </Switch>
+
+        <Footer />
+    </Router>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Reservation = () => (
+    <main>
+        <h2>Detail jízdenky</h2>
+    </main>
+);
